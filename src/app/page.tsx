4572c24b54feab936bf60542cb41a096dabac599
Juan Carlos Castro Cruz <jc.castroc@gmail.com>
2026-08-08 13:58:42 -0600

    feat: add landing value proposition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
+import ValueProposition from "../components/ValueProposition/ValueProposition";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
 
       <main>
         <Hero />
+        <ValueProposition />
       </main>
     </>
   );
